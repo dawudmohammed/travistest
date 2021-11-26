f389ab13235882b4d9b7b2cac8c1ae0bf2031c79
mohammeddawud <dawud.mohammed@turntabl.io>
2021-11-26 07:08:49 +0000

    details of header file added

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <html>
   <body>
     <h1>Hello world<h1>
-    <div style="width:500px;height:200px;background-color:orange;">
+    <div style="width:1000px;height:100px;background-color:orange;">
        </div>
 
   </body>
